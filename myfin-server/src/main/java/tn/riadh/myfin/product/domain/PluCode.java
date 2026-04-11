1f691299afa9913f23e8fd8b40c829746afd298a
riadhlaabidi <riadh.by.laabidi@gmail.com>
2026-04-12 00:04:17 +0100

    Add PLU

---
+++ myfin-server/src/main/java/tn/riadh/myfin/product/domain/PluCode.java
@@ -0,0 +1,51 @@
+package tn.riadh.myfin.product.domain;
+
+import java.util.Objects;
+
+import org.jmolecules.ddd.types.ValueObject;
+
+public class PluCode implements ValueObject {
+
+    private static final Integer MAX_PLU = 99999;
+    private final Integer value;
+
+    private PluCode(Integer value) {
+        Objects.requireNonNull(value, "PluCode cannot be null");
+
+        if (value < 0 || value > MAX_PLU) {
+            throw new IllegalArgumentException("PluCode outside range 0.." + MAX_PLU);
+        }
+
+        this.value = value;
+    }
+
+    PluCode of(Integer value) {
+        return new PluCode(value);
+    }
+
+    public Integer value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof PluCode)) {
+            return false;
+        }
+        PluCode other = (PluCode) obj;
+        return value == other.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+}
